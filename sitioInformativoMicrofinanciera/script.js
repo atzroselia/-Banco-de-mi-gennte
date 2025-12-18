cb6759f117fcf0a77ad535d73c81c529ae0728cc
Roselia Atz <estheratzmorales@gmail.com>
2025-12-18 12:55:07 -0600

    Add files via upload

---
+++ sitioInformativoMicrofinanciera/script.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.faq-question').forEach(button => {
+        button.addEventListener('click', () => {
+            const faqItem = button.parentElement;
+            faqItem.classList.toggle('active');
+        });
+    });
+});
+
+ 
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.faq-question').forEach(button => {
+        button.addEventListener('click', () => {
+            const faqItem = button.parentElement;
+            faqItem.classList.toggle('active');
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.faq-question').forEach(button => {
+        button.addEventListener('click', () => {
+            const faqItem = button.parentElement;
+            faqItem.classList.toggle('active');
+        });
+    });
+});
